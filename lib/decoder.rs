be36853ce4e58e909f1558927c174495c4cffbb2
arielpajares <37309592+arielpajares@users.noreply.github.com>
2024-01-24 22:24:41 -0300

    Add files via upload

---
+++ lib/decoder.rs
@@ -0,0 +1,55 @@
+use std::io::{self,Write,Read};
+use std::fs::File;
+
+pub struct Canvas {
+pub width: String,
+pub height: String
+}
+
+impl Canvas {
+fn file_to_string(self,cont: &str) -> String {
+let mut i1: u64 = 0;
+let mut i2:u64 =0;
+let mut final_cont = String::new();
+let mut s1 = String::new();
+
+for i in cont.chars() {
+
+if i == ',' {
+i1 = i1+1;
+match i1 {
+1 => final_cont = final_cont+&String::from("[".to_string()+&s1+&",".to_string()),
+8 => {final_cont = final_cont+&String::from(s1+&"],".to_string());i2 = i2+1;},
+9 => {final_cont = final_cont+&String::from("[".to_string()+&s1+&",".to_string());i1=1;},
+_ => final_cont = final_cont+&String::from(s1+&",".to_string())
+}
+s1 = String::from("");
+    }
+else {
+s1 = s1+&String::from(i);
+    }
+}
+final_cont = final_cont+&String::from(subtract_char("\n",&s1)+&String::from("],"));
+
+final_cont
+    }
+
+pub fn generate_js_file(self,name:&str,cont:&String){
+let dimensions = String::from("];width=".to_string()+&self.width+&";height=".to_string()+&self.height+&";".to_string());
+let final_cont = self.file_to_string(&cont);
+let js_var = "screen=[".to_string()+&final_cont+&dimensions;
+
+let mut filo = File::create(name).unwrap();
+filo.write(js_var.as_bytes()).unwrap();
+    }
+}
+
+fn subtract_char(x:&str,y:&str) -> String{
+let mut z = String::new();
+for i in y.chars() {
+if i.to_string() != x {
+z = z+&String::from(i);
+        }
+    }
+z
+}
