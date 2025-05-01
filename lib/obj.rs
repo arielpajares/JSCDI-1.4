9cb9cba3fa666a750c5a043ee4a5b7dad37c13b9
arielpajares <37309592+arielpajares@users.noreply.github.com>
2025-05-01 12:52:09 -0300

    Update obj.rs

--- lib/obj.rs
+++ lib/obj.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::{Read, Write};
 
-fn getObjVert(obj: &String) -> Vec<Vec<String>> {
+pub fn getObjVertices2D(obj: &String) -> Vec<Vec<String>> {
     let mut status = "READ";
-    let mut vertices: Vec<Vec<String>> = vec![];
-    let mut tempArray: Vec<String> = vec![];
+    let mut vertices: Vec<Vec<String>> = Vec::new();
+    let mut tempArray: Vec<String> = Vec::new();
     let mut tempString = String::new();
 
     for c in obj.chars() {
@@ -20,7 +20,7 @@ fn getObjVert(obj: &String) -> Vec<Vec<String>> {
                 status = "READ";
                 tempArray.remove(1);
                 vertices.push(tempArray);
-                tempArray = vec![];
+                tempArray = Vec::new();
             }
             else if c != ' ' {
                 tempString = format!("{tempString}{c}");
@@ -39,11 +39,120 @@ fn getObjVert(obj: &String) -> Vec<Vec<String>> {
     vertices
 }
 
-fn getObjF(obj: &String) -> Vec<String> {
+pub fn getObjVertices(obj: &String) -> Vec<Vec<String>> {
+    let mut status = "READ";
+    let mut vertices: Vec<Vec<String>> = Vec::new();
+    let mut tempArray: Vec<String> = Vec::new();
+    let mut tempString = String::new();
+
+    for c in obj.chars() {
+
+        if status == "GET_VERT" {
+            if c == ' ' && tempString != String::new() {
+                tempArray.push(tempString);
+                tempString = String::new();
+            }
+            else if c == '\n' {
+                tempArray.push(tempString);
+                tempString = String::new();
+                status = "READ";
+                vertices.push(tempArray);
+                tempArray = Vec::new();
+            }
+            else if c != ' ' {
+                tempString = format!("{tempString}{c}");
+            }
+        }
+        else if status == "READ" {
+            tempString = format!("{tempString}{c}");
+            if c == ' ' || c == '\n' {
+                if tempString == "v " {status = "GET_VERT";}
+                tempString = String::new();
+            }
+        }
+
+    }
+
+    vertices
+}
+
+pub fn getObjV(obj: &String) -> Vec<Vec<Vec<String>>> {
+    let mut status = "READ";
+    let mut vertices: Vec<Vec<String>> = Vec::new();
+    let mut uvCoords: Vec<Vec<String>> = Vec::new();
+    let mut normals: Vec<Vec<String>> = Vec::new();
+    let mut tempArray: Vec<String> = Vec::new();
+    let mut tempString = String::new();
+
+    for c in obj.chars() {
+
+        if status == "GET_VERT" {
+            if c == ' ' && tempString != String::new() {
+                tempArray.push(tempString);
+                tempString = String::new();
+            }
+            else if c == '\n' {
+                tempArray.push(tempString);
+                tempString = String::new();
+                status = "READ";
+                vertices.push(tempArray);
+                tempArray = Vec::new();
+            }
+            else if c != ' ' {
+                tempString = format!("{tempString}{c}");
+            }
+        }
+        else if status == "GET_UV" {
+            if c == ' ' && tempString != String::new() {
+                tempArray.push(tempString);
+                tempString = String::new();
+            }
+            else if c == '\n' {
+                tempArray.push(tempString);
+                tempString = String::new();
+                status = "READ";
+                uvCoords.push(tempArray);
+                tempArray = Vec::new();
+            }
+            else if c != ' ' {
+                tempString = format!("{tempString}{c}");
+            }
+        } else if status == "GET_NORM" {
+            if c == ' ' && tempString != String::new() {
+                tempArray.push(tempString);
+                tempString = String::new();
+            }
+            else if c == '\n' {
+                tempArray.push(tempString);
+                tempString = String::new();
+                status = "READ";
+                normals.push(tempArray);
+                tempArray = Vec::new();
+            }
+            else if c != ' ' {
+                tempString = format!("{tempString}{c}");
+            }
+        }
+        else if status == "READ" {
+            tempString = format!("{tempString}{c}");
+            if c == ' ' || c == '\n' {
+                if tempString == "v " {status = "GET_VERT";}
+                if tempString == "vt " {status = "GET_UV";}
+                if tempString == "vn " {status = "GET_NORM";}
+                tempString = String::new();
+            }
+        }
+
+    }
+
+    vec![vertices, uvCoords, normals]
+}
+
+pub fn getObjF(obj: &String) -> Vec<String> {
     let mut status = "READ";
     let mut barFlag = false;
-    let mut arrayF: Vec<String> = vec![];
-    let mut tempArray: Vec<String> = vec![];
+    let mut arrayF: Vec<String> = Vec::new();
+    let mut tempArray: Vec<String> = Vec::new();
     let mut tempString = String::new();
 
     for c in obj.chars() {
@@ -73,21 +182,109 @@ fn getObjF(obj: &String) -> Vec<String> {
     arrayF
 }
 
-pub fn getObjTriangles(fileAddr: &String) -> Vec<f32> {
+pub fn getObjFData(obj: &String) -> Vec<Vec<String>> {
+    let mut status = "READ";
+    let mut barFlag = false;
+    let mut verticesF: Vec<String> = Vec::new();
+    let mut uvCoordsF: Vec<String> = Vec::new();
+    let mut normalsF: Vec<String> = Vec::new();
+    let mut tempArray: Vec<String> = Vec::new();
+    let mut tempString = String::new();
+
+    for c in obj.chars() {
+
+        if status == "GET_F" {
+            if c == ' ' && tempString != String::new() {
+                tempArray.push(tempString);
+                tempString = String::new();
+            } 
+            else if c == '/' {
+                tempArray.push(tempString);
+                tempString = String::new();
+            }
+            else if c == '\n' {
+                tempArray.push(tempString);
+                tempString = String::new();
+                status = "READ";
+            }
+            else {
+                if c != ' ' && c != '/' {tempString = format!("{tempString}{c}");}
+            }
+        }
+
+
+        if c == 'f' {status = "GET_F";}
+
+    }
+
+    let mut i = 0;
+    for _ in 0..tempArray.len()/3  {
+        verticesF.push(tempArray[i].clone());
+        uvCoordsF.push(tempArray[i+1].clone());
+        normalsF.push(tempArray[i+2].clone());
+        i += 3;
+    }
+
+    vec![verticesF, uvCoordsF, normalsF]
+}
+
+pub fn getObjTriangles(fileAddr: &String, zSkip: bool) -> Vec<f32> {
     let mut file = File::open(fileAddr).unwrap();
     let mut obj = String::new();
     file.read_to_string(&mut obj);
 
-    let mut triangles: Vec<f32> = vec![];
-    let mut vertices: Vec<Vec<String>> = getObjVert(&obj);
+    let mut triangles: Vec<f32> = Vec::new();
+    let mut vertices: Vec<Vec<String>>;
+    if zSkip {vertices = getObjVertices2D(&obj);}
+    else {vertices = getObjVertices(&obj);}
     let mut arrayF: Vec<String> = getObjF(&obj);
 
-    println!("{:?}", arrayF);
-
-    for f in arrayF {
-        triangles.push(vertices[f.parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
-        triangles.push(vertices[f.parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+    if zSkip {
+        for f in arrayF {
+            triangles.push(vertices[f.parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
+            triangles.push(vertices[f.parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+        }
+    }
+    else {
+        for f in arrayF {
+            triangles.push(vertices[f.parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
+            triangles.push(vertices[f.parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+            triangles.push(vertices[f.parse::<usize>().unwrap()-1][2].parse::<f32>().unwrap());
+        }
     }
 
     triangles
+}
+
+pub fn getObjModel(fileAddr: &String) -> Vec<f32> {
+    let mut file = File::open(fileAddr).unwrap();
+    let mut obj = String::new();
+    file.read_to_string(&mut obj);
+
+    let mut model: Vec<f32> = Vec::new();
+    let arrayV = getObjV(&obj);
+    let arrayF = getObjFData(&obj);
+
+    let vertices = arrayV[0].clone();
+    let uvCoords = arrayV[1].clone();
+    let normals = arrayV[2].clone();
+
+    let verticesF = arrayF[0].clone();
+    let uvCoordsF = arrayF[1].clone();
+    let normalsF = arrayF[2].clone();
+
+    for i in 0..verticesF.len() {
+        model.push(vertices[verticesF[i].parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
+        model.push(vertices[verticesF[i].parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+        model.push(vertices[verticesF[i].parse::<usize>().unwrap()-1][2].parse::<f32>().unwrap());
+
+        model.push(uvCoords[uvCoordsF[i].parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
+        model.push(uvCoords[uvCoordsF[i].parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+
+        model.push(normals[normalsF[i].parse::<usize>().unwrap()-1][0].parse::<f32>().unwrap());
+        model.push(normals[normalsF[i].parse::<usize>().unwrap()-1][1].parse::<f32>().unwrap());
+        model.push(normals[normalsF[i].parse::<usize>().unwrap()-1][2].parse::<f32>().unwrap());
+    }
+
+    model
 }
